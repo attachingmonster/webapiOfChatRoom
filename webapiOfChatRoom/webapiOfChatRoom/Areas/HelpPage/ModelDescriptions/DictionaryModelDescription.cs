f64a535afd29ecc3c68cda4172445c5c66de3127
attachingmonster <3550462318@qq.com>
2019-05-31 23:04:22 +0800

    webapiOfChatRoom起步

---
+++ webapiOfChatRoom/webapiOfChatRoom/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace webapiOfChatRoom.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ webapiOfChatRoom/webapiOfChatRoom/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace webapiOfChatRoom.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
