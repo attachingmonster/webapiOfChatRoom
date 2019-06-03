8c1cfde4f123e16f1133132c937e1ef29f3ecdf8
zhg12581 <2634562735@qq.com>
2019-06-03 17:11:25 +0800

    聊天界面上加个刷新消息按钮事件

---
+++ webapiOfChatRoom/webapiOfChatRoom/Controllers/DataModelReturnController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webapiOfChatRoom.DAL;
+using webapiOfChatRoom.ViewModels;
+
+namespace webapiOfChatRoom.Controllers
+{
+    public class DataModelReturnController : ApiController
+    {
+        private AccountContext db = new AccountContext();
+        private UnitOfWork unitOfWork = new UnitOfWork();
+        [HttpGet]
+        [ActionName("GetDataModel")]
+        public List<ViewModelDataReturn> ViewModelData()
+        {
+            var data = (from us in unitOfWork.DataModelRepository.Get()
+                        select new ViewModelDataReturn { UserAccount = us.UserAccount, Message = us.Message }).ToList();
+            return data;
+        }
+    }
+}
